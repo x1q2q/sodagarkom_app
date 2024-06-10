41e36de846713d52973eb9433e3ef7629dac3c6b
rafiknurf <arafiknurf@gmail.com>
2024-06-10 10:52:07 +0700

    first blood

---
+++ README.md
@@ -0,0 +1,3 @@
+# sodagarkom_app
+
+Mobile apps (android) based with flutter to sell computer
---
+++ android/app/src/main/kotlin/com/example/sodagarkom_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sodagarkom_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
